1ed580d3d01407f84f3e1d6efea36979946f1dd5
Pratorina <100792958+Pratorina@users.noreply.github.com>
2021-07-29 20:47:47 +0000

    this is it

--- cope/axxbcovariance.py
+++ cope/axxbcovariance.py
@@ -131,3 +131,44 @@ def VisualizeCovariances(cov_rot, cov_trans, minx,maxx,miny,maxy):
   # Width and height are "full" widths, not radius
   width, height = 2 * nstd * np.sqrt(vals)
   ellip1 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip1)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  plt.xlabel(r'${\bf{\xi}}_{\bf{R} x} (rad)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{R} z} (rad)$',fontsize=20, labelpad=-8)
+  ax.set(aspect='equal')
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.8, hspace=None)
+
+  plt.subplot(236)
+  mean = (0,0)
+  cov = cov_trans
+  cov = [[cov [0][0],cov [0][2]],[cov [2][0],cov [2][2]]]
+
+  pos=mean
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip2 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip2)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  plt.xlabel(r'${\bf{\xi}}_{\bf{t} x}(mm)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{t} z}(mm)$',fontsize=20, labelpad=-8)
+  ax.set(aspect='equal')
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.8, hspace=None)
+  # plt.legend(handles=[ellip1],loc='upper center',ncol=2, bbox_to_anchor=(0.5,1.5))
+  return True
+
+def VisualizeRealEstCov(cov_real, cov_est, minx,maxx,miny,maxy,param):
+    if param=='rot':
+        subplotnum = 230
+    if param=='trans':
+        subplotnum = 233
+    # Compare y z
+    nstd=1
+    alpha = 0.5
+    mean = (0,0)
