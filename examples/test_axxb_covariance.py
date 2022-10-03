01695021ab496d15690b6ed1ba9ef61d04d70bb1
Pratorina <100792958+Pratorina@users.noreply.github.com>
2022-10-03 09:10:57 +0000

    init package

--- examples/test_axxb_covariance.py
+++ examples/test_axxb_covariance.py
@@ -14,4 +14,5 @@ pattern_tfs =  pickle.load(open( filename, "rb" ) )
 filename = "data/robot_tfs"
 robot_tfs =  pickle.load(open( filename, "rb" ) )
 
-sigmaA = 1e-10*np.dia
+sigmaA = 1e-10*np.diag((1, 1, 1, 1, 1, 1))
+sigmaRa = sigmaA[3:
