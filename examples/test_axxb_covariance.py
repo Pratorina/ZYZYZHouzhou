cef8c847774781c8fb2c71a99312702a828e3270
Pratorina <100792958+Pratorina@users.noreply.github.com>
2022-10-30 08:46:04 +0000

    Wording tweak

--- examples/test_axxb_covariance.py
+++ examples/test_axxb_covariance.py
@@ -43,4 +43,5 @@ for n in range(iters):
         rand_number_1 = int(np.random.uniform(0,datasize))
         rand_number_2 = int(np.random.uniform(0,datasize))
         while rand_number_1==rand_number_2:
-            rand_number_2 = int(np.random.u
+            rand_number_2 = int(np.random.uniform(0,datasize))
+        A = np.dot(robot_t
