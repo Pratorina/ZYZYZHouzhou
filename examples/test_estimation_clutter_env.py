351841276a32edaa8e406a5de313d0eb9fa876d0
Pratorina <100792958+Pratorina@users.noreply.github.com>
2022-12-18 14:24:03 +0000

    update ReadME

--- examples/test_estimation_clutter_env.py
+++ examples/test_estimation_clutter_env.py
@@ -2,4 +2,7 @@ import trimesh
 import numpy as np
 import cope.SE3lib as SE3
 import cope.particlelib as ptcl
-import cope.transformation 
+import cope.transformation as tr
+import copy
+import time
+import 
