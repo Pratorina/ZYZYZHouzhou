9c7511735dfdabca6f3141871c9826a159a79b6f
Pratorina <100792958+Pratorina@users.noreply.github.com>
2022-12-24 05:37:13 +0000

    Updated plugin version

--- examples/test_estimation_clutter_env.py
+++ examples/test_estimation_clutter_env.py
@@ -30,4 +30,5 @@ pos_err = 2e-3
 nor_err = 5./180.0*np.pi
 
 # Uncertainty & params
-sigma0 = np.diag([0.0001,0.0001,0.0001,0.09,0.09,0.
+sigma0 = np.diag([0.0001,0.0001,0.0001,0.09,0.09,0.09],0)
+sigma_desired = 0.09*np.diag([1e-6
