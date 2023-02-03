f7551a72d95708803f4c94a5a15443d64de3b5da
Pratorina <100792958+Pratorina@users.noreply.github.com>
2023-02-03 14:07:09 +0000

    Delint

--- examples/test_estimation_clutter_env.py
+++ examples/test_estimation_clutter_env.py
@@ -81,4 +81,5 @@ threshold = 3.  # a threshold value for determining when a data point fits a mod
 d = 7  # the number of good data values required to assert that a model fits well to data
 
 t0 = time.time()
-ransac_transformation, ransac_score, ransac_inliers_idx = ptcl.RansacParticle(n,k,threshold,d,mesh,angle_dict, ptcls0, measurements, pos_err, nor_err, M, sigma0, sigma_desired, prune_percentage,dim = 6,
+ransac_transformation, ransac_score, ransac_inliers_idx = ptcl.RansacParticle(n,k,threshold,d,mesh,angle_dict, ptcls0, measurements, pos_err, nor_err, M, sigma0, sigma_desired, prune_percentage,dim = 6, visualize = False)
+print 'Time', time.time
