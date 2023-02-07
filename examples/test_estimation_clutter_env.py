78b19477b28a5426516cefb0db17286956291b07
Pratorina <100792958+Pratorina@users.noreply.github.com>
2023-02-07 21:08:39 +0000

    Fix lint warnings.

--- examples/test_estimation_clutter_env.py
+++ examples/test_estimation_clutter_env.py
@@ -90,4 +90,4 @@ print 'Dist trans:'
 print np.linalg.norm(T_r[:3,3]-T[:3,3])
 print 'Dist rot:'
 print np.linalg.norm(SE3.RotToVec(np.dot(np.linalg.inv(T_r[:3,:3]),T[:3,:3])))
-print 'Numb
+print 'Number of Inliers Detected:', len(ransac_inliers_i
