4bc8a696c961c20ba6ea93db46e2e7367ee6d968
Pratorina <100792958+Pratorina@users.noreply.github.com>
2023-02-12 11:28:54 +0000

    Cleaning up README

--- examples/test_propagating_fusing_covariances.py
+++ examples/test_propagating_fusing_covariances.py
@@ -19,4 +19,5 @@ T3[:3,3] = np.array([0,0,-0.03])
 sigma3 = np.diag([tiny,tiny,tiny,tiny,tiny,0.1],0)
 
 T4 = np.eye(4)
-T4[:3,3] = np.array([0,0
+T4[:3,3] = np.array([0,0.14,0])
+sigma4 = np.diag([tiny,tiny,ti
