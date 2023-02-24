22a06995d8cfd345e4ba5fbb604dce1714df7f1c
Pratorina <100792958+Pratorina@users.noreply.github.com>
2023-02-24 21:38:20 +0000

    USB: gadget: android: android USB gadget improvements:

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='cope',
 	version='1.0.0',
 	description='Covariance-based Poses Estimation library',
 	author='Huy Nguyen',
-	author_email='
+	author_email='huy.nguyendinh09@gmail.com',
+     packages=['cope'])
